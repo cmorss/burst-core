204a65a223f7f5fa38036933255fec24c1dc5468
Al <admin@bocoup.com>
2010-07-30 09:51:10 -0400

    Linted. Added closure compressed ver.

--- burst-core.js
+++ burst-core.js
@@ -1,65 +1,40 @@
-/* Burst-Core
-   Copyright F1LT3R @ Bocoup
-   License: Call me - http://bocoup.com */
-(function(window){
+  /* Burst-Core
+     Copyright F1LT3R @ Bocoup
+     License: Call me - http://bocoup.com */
+(function(){
 
   // Array Sort
   //////////////////////////////////////////////////////////////////////////////
-  function sortNumber(a,b){ return a - b };
-  
+  function sortNumber(a,b){ return a - b; }
+
   // Easing
   //////////////////////////////////////////////////////////////////////////////
   var ease = {
-    step          : function(x,t,b,c,d){ if(t==d){return d;}else{return 1;} },
     linear        : function(x,t,b,c,d){ return c*t/d + b; },
-    inQuad        : function(x,t,b,c,d){ return c*(t/=d)*t + b; },
-    outQuad       : function(x,t,b,c,d){ return -c *(t/=d)*(t-2) + b; },
-    inOutQuad     : function(x,t,b,c,d){ if((t/=d/2) < 1) return c/2*t*t + b; return -c/2 * ((--t)*(t-2) - 1) + b; },
-    inCubic       : function(x,t,b,c,d){ return c*(t/=d)*t*t + b; },
-    outCubic      : function(x,t,b,c,d){ return c*((t=t/d-1)*t*t + 1) + b; },
-    inOutCubic    : function(x,t,b,c,d){ if((t/=d/2) < 1) return c/2*t*t*t + b; return c/2*((t-=2)*t*t + 2) + b; },
-    inQuart       : function(x,t,b,c,d){ return c*(t/=d)*t*t*t + b; },
-    outQuart      : function(x,t,b,c,d){ return -c * ((t=t/d-1)*t*t*t - 1) + b; },
-    inOutQuart    : function(x,t,b,c,d){ if((t/=d/2) < 1) return c/2*t*t*t*t + b; return -c/2 * ((t-=2)*t*t*t - 2) + b; },
-    inQuint       : function(x,t,b,c,d){ return c*(t/=d)*t*t*t*t + b; },
-    outQuint      : function(x,t,b,c,d){ return c*((t=t/d-1)*t*t*t*t + 1) + b; },
-    inOutQuint    : function(x,t,b,c,d){ if((t/=d/2) < 1) return c/2*t*t*t*t*t + b; return c/2*((t-=2)*t*t*t*t + 2) + b; },
-    inSine        : function(x,t,b,c,d){ return -c * Math.cos(t/d * (Math.PI/2)) + c + b; },
-    outSine       : function(x,t,b,c,d){ return c * Math.sin(t/d * (Math.PI/2)) + b; },
-    inOutSine     : function(x,t,b,c,d){ return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b; },
-    inExpo        : function(x,t,b,c,d){ return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b; },
-    outExpo       : function(x,t,b,c,d){ return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b; },
-    inOutExpo     : function(x,t,b,c,d){ if(t==0) return b; if (t==d) return b+c; if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b; return c/2 * (-Math.pow(2, -10 * --t) + 2) + b; },
-    inCirc        : function(x,t,b,c,d){ return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b; },
-    outCirc       : function(x,t,b,c,d){ return c * Math.sqrt(1 - (t=t/d-1)*t) + b; },
-    inOutCirc     : function(x,t,b,c,d){ if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b; return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b; },
-    inElastic     : function(x,t,b,c,d){ var s=1.70158;var p=0;var a=c; if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3; if (a < Math.abs(c)) { a=c; var s=p/4; } else var s = p/(2*Math.PI) * Math.asin (c/a); return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b; },
-    outElastic    : function(x,t,b,c,d){ var s=1.70158;var p=0;var a=c; if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3; if (a < Math.abs(c)) { a=c; var s=p/4; } else var s = p/(2*Math.PI) * Math.asin (c/a); return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b; },
-    inOutElastic  : function(x,t,b,c,d){ var s=1.70158;var p=0;var a=c; if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5); if (a < Math.abs(c)) { a=c; var s=p/4; } else var s = p/(2*Math.PI) * Math.asin (c/a); if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b; return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b; },
-    inBack        : function(x,t,b,c,d){ if(s == undefined) s = 1.70158; return c*(t/=d)*t*((s+1)*t - s) + b; },
-    outBack       : function(x,t,b,c,d){ if(s == undefined) s = 1.70158; return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b; },
-    inOutBack     : function(x,t,b,c,d){ if(s == undefined) s = 1.70158;  if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b; return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b; },
-    inBounce      : function(x,t,b,c,d){ return c - ease['outBounce']( x, d-t, 0, c, d) + b; },
-    outBounce     : function(x,t,b,c,d){ if((t/=d) < (1/2.75)){ return c*(7.5625*t*t) + b; } else if (t < (2/2.75)) { return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b; } else if (t < (2.5/2.75)) { return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b; } else { return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b; } },
-    inOutBounce   : function(x,t,b,c,d){ if(t < d/2) return ease['inBounce'](x, t*2, 0, c, d) * .5 + b; return ease['outBounce'](x, t*2-d, 0, c, d) * .5 + c*.5 + b; }
+    inOutQuad     : function(x,t,b,c,d){ if((t/=d/2) < 1){ return c/2*t*t + b;}else{ return -c/2 * ((--t)*(t-2) - 1) + b; }},
+    inOutCubic    : function(x,t,b,c,d){ if((t/=d/2) < 1){ return c/2*t*t*t + b;}else{ return c/2*((t-=2)*t*t + 2) + b; }},
+    inOutElastic  : function(x,t,b,c,d){ var s=1.70158;var p=0;var a=c; if (t===0){ return b;}  if ((t/=d/2)==2){ return b+c;}  if (!p){ p=d*(0.3*1.5);} if (a < Math.abs(c)) { a=c; s=p/4; } else{ s = p/(2*Math.PI) * Math.asin (c/a); if (t < 1){ return -0.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;}else{ return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*0.5 + c + b; }}},
+    inBounce      : function(x,t,b,c,d){ return c - ease.outBounce( x, d-t, 0, c, d) + b; },
+    outBounce     : function(x,t,b,c,d){ if((t/=d) < (1/2.75)){ return c*(7.5625*t*t) + b; } else if (t < (2/2.75)) { return c*(7.5625*(t-=(1.5/2.75))*t + 0.75) + b; } else if (t < (2.5/2.75)) { return c*(7.5625*(t-=(2.25/2.75))*t + 0.9375) + b; } else { return c*(7.5625*(t-=(2.625/2.75))*t + 0.984375) + b; } },
+    inOutBounce   : function(x,t,b,c,d){ if(t < d/2){ return ease.inBounce(x, t*2, 0, c, d) * 0.5 + b;}else{ return ease.outBounce(x, t*2-d, 0, c, d) * 0.5 + c*0.5 + b; }}
   };
 
   // Burst Instance
   //////////////////////////////////////////////////////////////////////////////
-  Burst = function Burst(){
-    this.name="Burst Instance";
+  var Burst = function Burst(){
     this.timelines={};
     this.loaded={};
     this.fps = 30;    
+    this.onframe=undefined;
   };
   Burst.prototype.timeline = function(name,start,end,speed,loop,callback){
     return this.timelines[name]||(arguments.length>1?this.timelines[name]=new Timeline(name,start,end,speed,loop,callback,this):undefined);
   };
   Burst.prototype.load = function( name ){
     return this.loaded[name] || (function(){
-      for( i in this.timelines ){
+      for(var i in this.timelines ){
         if( this.timelines[i] ){            
-          return this.loaded[i] = this.timelines[i];
+          return (this.loaded[i] = this.timelines[i]);
         }else{
           return this;
         }
@@ -71,23 +46,26 @@
   };
   Burst.prototype.play = function(){
     var deepref = this;
-    this.interval = setInterval(function(){
+    this.interval = window.setInterval(function(){
       deepref.frame();
     }, 1000 / this.fps );
   };
   Burst.prototype.frame = function( frame ){
+    if(this.onframe){this.onframe();}
     for( var i in this.loaded ){
-      this.loaded[i].play( frame );
+      if(this.hasOwnProperty("loaded")){
+        this.loaded[i].play( frame );
+      }
     }
   };
   Burst.prototype.stop = function(){
-    clearInterval( this.interval );
+    window.clearInterval( this.interval );
     delete this.interval;
   };
 
   // Timeline
   //////////////////////////////////////////////////////////////////////////////
-  Timeline = function Timeline(name,start,end,speed,loop,callback,parent){
+  var Timeline = function Timeline(name,start,end,speed,loop,callback,parent){
     this.name=name;
     this.start=this.frame=start;
     this.end=end;
@@ -104,28 +82,30 @@
   Timeline.prototype.play = function( frame ){
     this.frame = frame || (this.frame += this.speed);
     if( this.loop ){
-      if( this.frame >= this.end ){ this.frame = this.start; }
-      if( this.frame <= this.start ){ this.frame = this.end; }
+      if( this.frame > this.end ){ this.frame = this.start; }
+      if( this.frame < this.start ){ this.frame = this.end; }
     }else{
       if( this.frame >= this.end){
         this.frame = this.end;
         this.parent.unload(this.name);
-        this.callback?this.callback():0;
+        if(this.callback){this.callback();}
       }
       if( this.frame <= this.start ){
         this.frame = this.start;
         this.parent.unload(this.name);
-        this.callback?this.callback():0;
+        if(this.callback){this.callback();}
       }
     }
     for( var j in this.tracks ){
-      this.tracks[j].play( this.frame );
+      if(this.hasOwnProperty("tracks")){
+        this.tracks[j].play( this.frame );
+      }
     }
   };  
 
   // Track
   //////////////////////////////////////////////////////////////////////////////
-  Track = function Track(name,objRef,prop,parent){
+  var Track = function Track(name,objRef,prop,parent){
     this.name=name;
     this.objRef=objRef;
     this.prop=prop;
@@ -142,11 +122,11 @@
     }
     if(arguments.length>1){
       var keyIndex=[], keyStack=[], thisKey = this.keys[this.keys.length] = new Key(frame,value,ease,this);
-      for(var i=0;i<this.keys.length;i++){
+      for(i=0;i<this.keys.length;i++){
         keyIndex[i]=this.keys[i].frame;
       }
       keyIndex.sort(sortNumber);      
-      for(var i=0;i<this.keys.length;i++){
+      for(i=0;i<this.keys.length;i++){
         for(var j=0;j<this.keys.length;j++){
           if(keyIndex[i]==this.keys[j].frame){
             keyStack[i]=this.keys[j];
@@ -154,7 +134,7 @@
         }
       }
       this.keys=[];
-      for(var i=0, l=keyStack.length; i< l; i++){
+      for(i=0, l=keyStack.length; i< l; i++){
         this.keys[i] = keyStack[i];
       }
       return thisKey;
@@ -185,10 +165,10 @@
 
   // Key
   //////////////////////////////////////////////////////////////////////////////
-  Key = function Key(frame,value,ease,parent){
+  var Key = function Key(frame,value,ease,parent){
     this.frame=frame;
     this.value=value;
-    this.ease=ease;
+    this.ease=ease||'linear';
     this.parent=parent;
     return this;
   };
@@ -200,9 +180,9 @@
     var track = this.parent;
     return track.key.call(track,name,objRef);
   };
-  
+
   // Instantiation
   //////////////////////////////////////////////////////////////////////////////
   window.burst = new Burst();
-  
-})(window);
+
+})();
